8ecc5455e289325c141a9f948ce9b706dbd92c6e
MCraftYT25 <93701162+MCraftYT25@users.noreply.github.com>
2023-10-03 19:08:53 +0200

    Add files via upload
    V0.4

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
 const year = "2023";
 const date = "2023-10-02"
 const version = 0.3;
-document.getElementsByTagName("footer")[0].innerHTML = "<div>" + "<p>" + year + "</p>" + '<p class="version">V' + version + "</p>" + "</div>";
+document.getElementsByTagName("footer")[0].innerHTML = "<div>" + "<span>" + year + "</span>" + '<span class="version"> V' + version + "</span>" + "</div>";
